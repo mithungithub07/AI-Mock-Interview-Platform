2afc9f9bca79d1f5f554040f0fb59c4d4c98d20a
mithungithub07 <mithun.kumar.careers@gmail.com>
2026-03-16 15:46:19 +0530

    added new Vercel URL to CORS

--- backend/main.py
+++ backend/main.py
@@ -8,6 +8,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
+        "https://ai-mock-interview-platform-mu.vercel.app", #New ONe
         "https://ai-mock-interview-platform-bymithun.vercel.app",
         "https://ai-mock-interview-platform-git-618cb6-mithungithub07s-projects.vercel.app",
         "https://ai-mock-interview-platform-bymithun-f7p5p5jxr.vercel.app",
