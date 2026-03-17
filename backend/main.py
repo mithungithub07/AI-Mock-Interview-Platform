4ae2804109ce466984e495a45a59b2e6b90616c7
mithungithub07 <mithun.kumar.careers@gmail.com>
2026-03-17 15:36:19 +0530

    Added Two Urls

--- backend/main.py
+++ backend/main.py
@@ -7,7 +7,9 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://ai-mock-interview-platform-mu.vercel.app",
+                   "http://localhost:5173"
+                   ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
