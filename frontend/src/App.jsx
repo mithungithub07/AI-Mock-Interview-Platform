26b56b87ea8a06eb28d7eb8da2ef3370c44c1eae
mithungithub07 <mithun.kumar.careers@gmail.com>
2026-04-06 16:07:02 +0530

    Frontend app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,8 @@ import { Routes, Route } from "react-router-dom"
 import HomePage from "../components/HomePage"
 import Interview from "../components/Interview"
 import Feedback from "../components/Feedback"
+import Login from "../components/Login"
+import Register from "../components/Register"
 
 
 function App() {
@@ -15,6 +17,10 @@ function App() {
         <Route path="/interview" element={<Interview />} />
 
         <Route path="/feedback" element={<Feedback />} />
+
+        <Route path="/login" element={<Login />} />
+
+        <Route path="/register" element={<Register />} />
 
       </Routes>
 
