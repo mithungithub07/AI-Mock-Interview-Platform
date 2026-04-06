e81ab52f198abfa685ba6f409f2ea4930cc6adb3
mithungithub07 <mithun.kumar.careers@gmail.com>
2026-04-06 15:42:14 +0530

    fix: import auth.css

--- frontend/components/Login.jsx
+++ frontend/components/Login.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
 import { loginUser } from '../services/api';
-import '../style/auth.css';
+import "../style/auth.css";
 
 const Login = () => {
     const [email, setEmail] = useState('');
