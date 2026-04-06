c6dbe225d69de21e54371af9bc329173a33c459e
mithungithub07 <mithun.kumar.careers@gmail.com>
2026-04-06 16:14:42 +0530

    fix: api.js

--- frontend/services/api.js
+++ frontend/services/api.js
@@ -24,3 +24,16 @@ export const generateFeedback = async (role, interviewData) => {
 
   return response.data
 }
+
+
+// Register
+export const registerUser = async (data) => {
+  const response = await API.post("/register", data)
+  return response.data
+}
+
+// Login
+export const loginUser = async (data) => {
+  const response = await API.post("/login", data)
+  return response.data
+}
