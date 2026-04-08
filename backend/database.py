c0785d2b3d72424063b43412cf6590359c0e00b7
mithungithub07 <mithun.kumar.careers@gmail.com>
2026-04-08 12:22:03 +0530

    database.py requirements.txt, auth.py

--- backend/database.py
+++ backend/database.py
@@ -8,8 +8,10 @@ from dotenv import load_dotenv
 load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
+# print("DATABASE URL:", DATABASE_URL)
 
-engine = create_engine(DATABASE_URL)
+#engine = create_engine(DATABASE_URL)
+engine = create_engine(DATABASE_URL, connect_args={"sslmode": "require"})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
