4a27af4bbf326dd98e7f507c72e5f3c30cb39362
mithungithub07 <mithun.kumar.careers@gmail.com>
2026-04-16 11:03:31 +0530

    Modify: NavBar.css

--- frontend/components/NavBar.jsx
+++ frontend/components/NavBar.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
-import './Navbar.css';
+import "../style/navbar.css"
 
 const Navbar = () => {
     const navigate = useNavigate();
